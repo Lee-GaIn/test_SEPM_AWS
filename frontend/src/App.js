1c3a3d61d08a84bd5d6747b83ddc396fddf5efb1
Lee Gain <adore0126@gmail.com>
2022-08-28 17:11:58 +0700

    .

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import MainNav from "./pages/MainNav";
+import { theme } from "./lib/style/theme";
+import GlobalCSS from "./lib/style/GlobalCSS";
+
+/* Context */
+import { CurrentUserProvider } from "./contexts/CurrentUser";
+
+const AppContainer = styled.div`
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	overflow: hidden;
+`;
+
+function App() {
+	return (
+		<div className='App'>
+			<GlobalCSS />
+			<ThemeProvider theme={theme}>
+				<CurrentUserProvider>
+					<AppContainer>
+						<MainNav />
+					</AppContainer>
+				</CurrentUserProvider>
+			</ThemeProvider>
+		</div>
+	);
+}
+
+export default App;
